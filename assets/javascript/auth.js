72fe5de614c45fe1d29e521daebf943feba529e5
gjb117 <gregbrodzik@gmail.com>
2018-01-17 17:32:52 -0700

    add logic for some map functionality and authetnication

---
+++ assets/javascript/auth.js
@@ -0,0 +1,59 @@
+
+var config = {
+  apiKey: "AIzaSyBjhon0-cIYtELUMFxUT0isynUMCxaNp9Y",
+  authDomain: "food-truckr.firebaseapp.com",
+  databaseURL: "https://food-truckr.firebaseio.com",
+  projectId: "food-truckr",
+  storageBucket: "food-truckr.appspot.com",
+  messagingSenderId: "463131017710"
+};
+
+firebase.initializeApp(config);
+
+//Test data
+var email = "gregbrod@gmail.com";
+var password = "testpassword22"
+var displayName =  "gregggyyy";
+var newUser = true;
+
+var database = firebase.database();
+
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
+var usersRef = firebase.database().ref("users");
+
+//Adding to verify client is connecting to app before authentication
+connectedRef.on("value", function(snap) {
+  if (snap.val()) {
+
+    var con = connectionsRef.push(true);
+    con.onDisconnect().remove();
+  }
+});
+
+//the event listener should be once email and password submitted(after veryifying OK) and
+//should pull from form. This is just to test whether we may create a new account on firebase.
+$(".nav-item").on("click", function() {
+  alert("called");
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    console.log(error.code);
+    console.log(error.msg);
+  });
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+  alert("auth status changed")
+  if (user) {
+
+    usersRef.child(user.uid).set({
+      displayName: displayName,
+      uid: user.uid,
+      email: user.email
+    });
+
+  console.log("The user is singed in");
+  } else {
+  console.log("The user is not singed in");
+  }
+});
--- assets/javascript/jojo.js
+++ assets/javascript/jojo.js
@@ -1,6 +1,6 @@
 var userMapInput={lat: 39.710850, lng: -105.081505};
 
-function initMap() {
+/*function initMap() {
         var uluru = {lat: 39.742043, lng: -104.991531};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 12,
@@ -14,9 +14,9 @@ function initMap() {
           position: userMapInput,
           map: map
         });
-      };
+      };*/
 
-    var userInputSearch="";
+  var userInputSearch="";
 	var queryUrl = "https://api.foursquare.com/v2/venues/explore?client_id=NCFDMIMRBLCB1MNHTEXODLIWB2KWDVQBQ50ZTCYQ3X05N43Y&client_secret=JPMDM31IHWHU35E4EVKGG3BGVYWEZ4DHWHIHDP4YSQFBYSOB&v=20170801&ll=39.742043, -104.991531&query=food truck " + userInputSearch + "&limit=20";
 
 	$.ajax({
@@ -27,8 +27,8 @@ function initMap() {
 		// console.log(response.response.groups[0].items[0].venue);
 
 		var locationsArray= response.response.groups[0].items;
-		var newLocationsArray= _.sortBy(locationsArray.)
-		console.log(locationsArray);
+		//var newLocationsArray= _.sortBy(locationsArray);
+		//console.log(locationsArray);
 
 		for(var i=0; i< locationsArray.length; i++){
 			// console.log(locationsArray[i].venue);
