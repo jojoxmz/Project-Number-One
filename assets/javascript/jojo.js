8ae7d177f9b27c52a61cccaca99fee8845b98a57
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-01-17 18:17:51 -0700

    added current location

--- assets/javascript/jojo.js
+++ assets/javascript/jojo.js
@@ -6,6 +6,8 @@ function initMap() {
           zoom: 12,
           center: uluru
         });
+        infoWindow = new google.maps.InfoWindow;
+
         var marker = new google.maps.Marker({
           position: uluru,
           map: map
@@ -15,6 +17,34 @@ function initMap() {
           map: map
         });
 
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        } 
+
+      };
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
       };
 
 
