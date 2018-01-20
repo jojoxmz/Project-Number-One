44c23e4ad7d4ec2be03b4a48fd9969eadcf3bfe9
JoJo Zheng^C <jojozheng1216@hotmail.com>
2018-01-20 15:08:58 -0700

    added ajax'

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -30,36 +30,51 @@ function displayTruckInfo(){
       console.log(response);
 
       // for (var i = 0; i < 6; i++) {
+    const sortedRatings = _.sortBy(response.businesses, function(business) {
+        return - business.rating;
+      });
 
-      var truck1Thumbnail = $("#truck-1-thumbnail").attr("src", response.businesses[0].image_url);
-      var truck1Name = $("#truck-1-name").text(response.businesses[0].name);
-      var truck1Image = $("#truck-1-image").attr("src", response.businesses[0].image_url);
-      var truck1Rating = $("#truck-1-rating").text("Rating " + response.businesses[0].rating);
+      console.log('hoopefully sorted array', sortedRatings);
 
-      var truck2Thumbnail = $("#truck-2-thumbnail").attr("src", response.businesses[1].image_url);
-      var truck2Name = $("#truck-2-name").text(response.businesses[1].name);
-      var truck2Image = $("#truck-2-image").attr("src", response.businesses[1].image_url);
-      var truck2Rating = $("#truck-2-rating").text("Rating " + response.businesses[1].rating);
+      let i = 0;
+      while (i <= 6) {
+        $(`#truck-${i + 1}-thumbnail`).attr("src", sortedRatings[i].image_url);
+        $(`#truck-${i + 1}-name`).text(sortedRatings[i].name);
+        $(`#truck-${i +1}-image`).attr("src", sortedRatings[i].image_url);
+        $(`#truck-${i+1}-rating`).text("Rating " + sortedRatings[i].rating);
+        i++
+      }
+console.log(response.businesses)
+console.log(sortedRatings)
+      // var truck1Thumbnail = $("#truck-1-thumbnail").attr("src", response.businesses[0].image_url);
+      // var truck1Name = $("#truck-1-name").text(response.businesses[0].name);
+      // var truck1Image = $("#truck-1-image").attr("src", response.businesses[0].image_url);
+      // var truck1Rating = $("#truck-1-rating").text("Rating " + response.businesses[0].rating);
 
-      var truck3Thumbnail = $("#truck-3-thumbnail").attr("src", response.businesses[2].image_url);
-      var truck3Name = $("#truck-3-name").text(response.businesses[2].name);
-      var truck3Image = $("#truck-3-image").attr("src", response.businesses[2].image_url);
-      var truck3Rating = $("#truck-3-rating").text("Rating " + response.businesses[2].rating);
+      // var truck2Thumbnail = $("#truck-2-thumbnail").attr("src", response.businesses[1].image_url);
+      // var truck2Name = $("#truck-2-name").text(response.businesses[1].name);
+      // var truck2Image = $("#truck-2-image").attr("src", response.businesses[1].image_url);
+      // var truck2Rating = $("#truck-2-rating").text("Rating " + response.businesses[1].rating);
 
-      var truck4Thumbnail = $("#truck-4-thumbnail").attr("src", response.businesses[3].image_url);
-      var truck4Name = $("#truck-4-name").text(response.businesses[3].name);
-      var truck4Image = $("#truck-4-image").attr("src", response.businesses[3].image_url);
-      var truck4Rating = $("#truck-4-rating").text("Rating " + response.businesses[3].rating);
+      // var truck3Thumbnail = $("#truck-3-thumbnail").attr("src", response.businesses[2].image_url);
+      // var truck3Name = $("#truck-3-name").text(response.businesses[2].name);
+      // var truck3Image = $("#truck-3-image").attr("src", response.businesses[2].image_url);
+      // var truck3Rating = $("#truck-3-rating").text("Rating " + response.businesses[2].rating);
 
-      var truck5Thumbnail = $("#truck-5-thumbnail").attr("src", response.businesses[6].image_url);
-      var truck5Name = $("#truck-5-name").text(response.businesses[6].name);
-      var truck5Image = $("#truck-5-image").attr("src", response.businesses[6].image_url);
-      var truck5Rating = $("#truck-5-rating").text("Rating " + response.businesses[6].rating);
+      // var truck4Thumbnail = $("#truck-4-thumbnail").attr("src", response.businesses[3].image_url);
+      // var truck4Name = $("#truck-4-name").text(response.businesses[3].name);
+      // var truck4Image = $("#truck-4-image").attr("src", response.businesses[3].image_url);
+      // var truck4Rating = $("#truck-4-rating").text("Rating " + response.businesses[3].rating);
 
-      var truck6Thumbnail = $("#truck-6-thumbnail").attr("src", response.businesses[5].image_url);
-      var truck6Name = $("#truck-6-name").text(response.businesses[5].name);
-      var truck6Image = $("#truck-6-image").attr("src", response.businesses[5].image_url);
-      var truck6Rating = $("#truck-6-rating").text("Rating " + response.businesses[5].rating);
+      // var truck5Thumbnail = $("#truck-5-thumbnail").attr("src", response.businesses[6].image_url);
+      // var truck5Name = $("#truck-5-name").text(response.businesses[6].name);
+      // var truck5Image = $("#truck-5-image").attr("src", response.businesses[6].image_url);
+      // var truck5Rating = $("#truck-5-rating").text("Rating " + response.businesses[6].rating);
+
+      // var truck6Thumbnail = $("#truck-6-thumbnail").attr("src", response.businesses[5].image_url);
+      // var truck6Name = $("#truck-6-name").text(response.businesses[5].name);
+      // var truck6Image = $("#truck-6-image").attr("src", response.businesses[5].image_url);
+      // var truck6Rating = $("#truck-6-rating").text("Rating " + response.businesses[5].rating);
 
 
     });
@@ -71,10 +86,8 @@ displayTruckInfo();
 var truckIdArray = ["east-coast-joes-denver", "the-gyros-king-food-truck-denver", "stella-blue-food-truck-denver", "rocky-mountain-slices-denver", "ba-nom-a-nom-denver", "flex-able-food-trucks-denver"];
 
 function displayTruckReviews() {
-
-  truckIdArray.forEach (function(truckId, i) {
-
-    fetch(`${corsHeroku}https://api.yelp.com/oauth2/token?client_id=${client_id}&client_secret=${client_secret}`, {
+truckIdArray.forEach(function(truckId, i) {
+    return fetch(`${corsHeroku}https://api.yelp.com/oauth2/token?client_id=${client_id}&client_secret=${client_secret}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -97,11 +110,11 @@ function displayTruckReviews() {
 
         var truckReview = $("#truck-" + (i+1) + "-reviews").text("Reviews: " + response.reviews[0].text + '<br>' + response.reviews[1].text)
         // + '<br>' + response.reviews[2].text);
-
       });
     });
   });
 }
+
 displayTruckReviews();
 
 
