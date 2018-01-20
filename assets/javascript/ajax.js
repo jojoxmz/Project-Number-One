499ec184b18c14c0550633c40d178b4d9fd2adab
Scott Thomas <scottthomas@Scotts-MacBook-Pro.local>
2018-01-20 10:46:20 -0700

    started search on click function

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -1,6 +1,8 @@
 // Creating our ajax call for yelp API
 
 
+
+
 const client_id = 'UBoDtdxOKSgJELPqsAtwag';
 const client_secret = 'DluMc4r2kSvoSRdksiaNDNqkXiA9fFJKRPPWFruza63FtNGeRJrYaqCIN3StcVNZ';
 const corsHeroku = 'https://cors-anywhere.herokuapp.com/';
@@ -70,7 +72,43 @@ var truckIdArray = ["east-coast-joes-denver", "the-gyros-king-food-truck-denver"
 
 function displayTruckReviews() {
 
-truckIdArray.forEach (function(truckId, i) {
+  truckIdArray.forEach (function(truckId, i) {
+
+    fetch(`${corsHeroku}https://api.yelp.com/oauth2/token?client_id=${client_id}&client_secret=${client_secret}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({ grant_type: 'client_credentials' })
+    })
+    .then((response) => response.json())
+    .then((responseJSON) => responseJSON.access_token)
+    .then((accessToken) => {
+      fetch(corsHeroku + "https://api.yelp.com/v3/businesses/" + truckId + "/reviews", {
+        method: 'GET',
+        headers: {
+          'authorization': `Bearer ${accessToken}`,
+        }
+      })
+      .then(response =>  response.json())
+      .then(response => {
+        console.log(response);
+
+
+        var truckReview = $("#truck-" + (i+1) + "-reviews").text("Reviews: " + response.reviews[0].text + '<br>' + response.reviews[1].text)
+        // + '<br>' + response.reviews[2].text);
+
+      });
+    });
+  });
+}
+displayTruckReviews();
+
+
+$("#truck-query").on("click", function() {
+  event.preventDefault();
+
+  var searchTerm = $("#search-term").val().trim();
 
   fetch(`${corsHeroku}https://api.yelp.com/oauth2/token?client_id=${client_id}&client_secret=${client_secret}`, {
     method: 'POST',
@@ -82,7 +120,7 @@ truckIdArray.forEach (function(truckId, i) {
   .then((response) => response.json())
   .then((responseJSON) => responseJSON.access_token)
   .then((accessToken) => {
-    fetch(corsHeroku + "https://api.yelp.com/v3/businesses/" + truckId + "/reviews", {
+    fetch(corsHeroku + "https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&categories=foodtrucks&latitude=39.742043&longitude=-104.991531", {
       method: 'GET',
       headers: {
         'authorization': `Bearer ${accessToken}`,
@@ -90,14 +128,9 @@ truckIdArray.forEach (function(truckId, i) {
     })
     .then(response =>  response.json())
     .then(response => {
+      console.log(searchTerm);
       console.log(response);
-
-
-      var truckReview = $("#truck-" + (i+1) + "-reviews").text("Reviews: " + response.reviews[0].text + '<br>' + response.reviews[1].text)
-       // + '<br>' + response.reviews[2].text);
 
     });
   });
 });
-}
-displayTruckReviews();
